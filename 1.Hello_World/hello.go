a63a914f96026661bb75ee00480292f0c310761b
karthick narayanan <karthickn210@gmail.com>
2020-10-04 17:59:05 +0530

    program file commit

---
+++ 1.Hello_World/hello.go
@@ -0,0 +1,13 @@
+//Declare a main package (a package is a way to group functions).
+package main
+
+//Package fmt implements formatted I/O with functions analogous to C's printf and scanf. 
+//The format 'verbs' are derived from C's but are simpler.
+import "fmt"
+
+//Implement a main function to print a message to the console. 
+//A main function executes by default when you run code in the file.
+func main() {
+	//https://golang.org/pkg/fmt/
+	fmt.Println("Hello Karthick")
+}
